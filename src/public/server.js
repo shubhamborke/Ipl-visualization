9c10c7bd1db7d37ea68ab112d30b9136ecf92218
shubhamborke7778 <shubham.borke.17@mountblue.tech>
2021-09-20 23:26:31 +0530

    Modify server for erro handling

--- src/public/server.js
+++ src/public/server.js
@@ -2,6 +2,10 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
+/*
+    Path for json file datas
+*/
+
 const matchesPerYear = path.join(__dirname,"/output/matchesPerYear.json");
 const bestEconomyPlayers = path.join(__dirname,"/output/bestEconomyPlayers.json");
 const extraRunIn2016 = path.join(__dirname,"/output/extraRunIn2016.json");
@@ -11,123 +15,138 @@ const tossAndMachesWinner = path.join(__dirname,"/output/tossAndMachesWinner.jso
 const dismissalPlayer = path.join(__dirname,"/output/dismissalPlayer.json");
 const matchesWonPerYear = path.join(__dirname,"/output/matchesWonPerYear.json");
 
+/* 
+    Creation of server
+*/
+
 const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/matchesPerYear.json":
-      fs.readFile(matchesPerYear, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*' });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/json" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/bestEconomyPlayers.json":
-      fs.readFile(bestEconomyPlayers, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/json" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/extraRunIn2016.json":
-      fs.readFile(extraRunIn2016, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/json" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/mostMatchAward.json":
-      fs.readFile(mostMatchAward, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/json" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/top10EconomiBowler.json":
-      fs.readFile(top10EconomiBowler, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/json" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/tossAndMachesWinner.json":
-      fs.readFile(tossAndMachesWinner, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/json" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/dismissalPlayer.json":
-      fs.readFile(dismissalPlayer, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/json" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/matchesWonPerYear.json":
-      fs.readFile(matchesWonPerYear, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/json" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/index.js":
-      fs.readFile(path.join(__dirname,"/index.js"), "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json" });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "application/javascript" });
-          res.end(data);
-        }
-      });
-      break;
-    case "/index.html":
-      console.log(path.join(__dirname,"index.html"))
-      fs.readFile(path.join(__dirname,"index.html"), "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "content-type": "application/json" });
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "content-type": "text/html" });
-          res.end(data);
-        }
-      });
-      break;
-    default:
-      res.end("Invalid address");
+  try{
+    switch (req.url) {
+      case "/matchesPerYear.json":
+        fs.readFile(matchesPerYear, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*' });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/bestEconomyPlayers.json":
+        fs.readFile(bestEconomyPlayers, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/extraRunIn2016.json":
+        fs.readFile(extraRunIn2016, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/mostMatchAward.json":
+        fs.readFile(mostMatchAward, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/top10EconomiBowler.json":
+        fs.readFile(top10EconomiBowler, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/tossAndMachesWinner.json":
+        fs.readFile(tossAndMachesWinner, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/dismissalPlayer.json":
+        fs.readFile(dismissalPlayer, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/matchesWonPerYear.json":
+        fs.readFile(matchesWonPerYear, "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json", "Access-Control-Allow-Origin": '*'  });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/index.js":
+        fs.readFile(path.join(__dirname,"/index.js"), "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json" });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "application/javascript" });
+            res.end(data);
+          }
+        });
+        break;
+      case "/index.html":
+        console.log(path.join(__dirname,"index.html"))
+        fs.readFile(path.join(__dirname,"index.html"), "utf-8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "content-type": "application/json" });
+            res.end("File not found");
+          } else {
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(data);
+          }
+        });
+        break;
+      default:
+        res.end("Invalid address");
+    }
+
+  }catch(err){
+    document.write(err)
   }
+  
 });
+
+
+/*
+    Listening of servers
+*/
 
 const port = process.env.PORT || 8000
 server.listen(port, (err) => {
